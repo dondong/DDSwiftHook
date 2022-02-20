785e86050d57c51fca7c82377ff6cdf4efb8e515
董明东 <dongmingdong@dongmingdongdeMacBook-Pro-2.local>
2022-02-21 00:03:35 +0800

    create

---
+++ DDSwiftHookDemo/DDSwiftHookDemo/DDSwiftHook.swift
@@ -0,0 +1,18 @@
+//
+//  DDSwiftHook.swift
+//  DDSwiftHookDemo
+//
+//  Created by 董明东 on 2022/2/20.
+//
+
+import Foundation
+
+class DDSwiftHook {
+    public static func replace(dstClass: AnyClass, dstMethodIndex: Int, srcClass: AnyClass, srcMethodIndex: Int) -> Bool {
+        return false;
+    }
+    
+    public static func replace(swiftClass: AnyClass) {
+        
+    }
+}
---
+++ DDSwiftHookDemo/DDSwiftHookDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DDSwiftHookDemo
+//
+//  Created by 董明东 on 2022/2/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
